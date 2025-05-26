a24144150e734186df9594055d33e85a21498037
spare1the1governor <468130@niuitmo.ru>
2025-05-26 11:01:45 +0300

    done

--- src/pages/HomeScreen.jsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage(){
-    return (
-        <div>
-            <h1>Добро пожаловать на игровой каталог !</h1>
-            <h3>Список игр: </h3>
-        </div>
-    )
-}
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div className="container">
-        <img src="https://gay0day.com/contents/videos_screenshots/285000/285847/preview_720p.mp4.jpg"
-            width="100%" height="100%" />
-      </div>
-    )
-  }
-
